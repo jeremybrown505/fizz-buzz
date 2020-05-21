8aa972a6d27d818033303f969fd08ffc8a94dede
Jeremy Brown <Jeremy505brown@gmail.com>
2020-05-21 14:27:36 -0600

    Fizzbuzz status in a string.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -3,18 +3,21 @@ package edu.cnm.deepdive;
 public class FizzBuzz {
 
   public static void main(String[] args) {
-    for (int counter= 0; counter <= 100; counter++) {
+    for (int counter = 1; counter <= 100; counter++) {
+      String status = "";
       boolean counterIsFizz = (counter % 3 == 0);
       boolean counterIsBuzz = (counter % 5 == 0);
-      if (counterIsFizz && counterIsBuzz) {
-        System.out.println("Fizzbuzz");
-      } else if (counterIsFizz) {
-        System.out.println("Fizz");
-      } else if (counterIsBuzz) {
-        System.out.println("Buzz");
-      } else {
-        System.out.println(counter);
-          }
-        }
+      if (counterIsFizz) {
+        status += "fizz";
       }
+      if (counterIsBuzz) {
+        status += "buzz";
+      }
+      if (status.isEmpty()) {
+        status += counter;
+      }
+      System.out.println(status);
+    }
+  }
+
 }
